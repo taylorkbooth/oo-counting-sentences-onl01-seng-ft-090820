38b86e249d9966f1f8ce55368400d299ee17df27
Taylor Booth <taylorkbooth@gmail.com>
2020-09-20 14:48:57 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,18 +4,14 @@ class String
 
   def sentence?
     self.end_with?(".")
-    
   end
 
   def question?
     self.end_with?("?")
-  
-
   end
 
   def exclamation?
     self.end_with?("!")
-
   end
 
   def count_sentences
